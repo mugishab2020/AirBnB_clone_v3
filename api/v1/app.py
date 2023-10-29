bff06b892511cc44ae3dc697efc6d887a473c3cc
mugishab2020 <mugishab2020@gmail.com>
2023-10-29 12:03:45 -0700

    start of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ importing the needed moules """
+
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -15,7 +17,7 @@ def teardown(exception):
     storage.close()
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
