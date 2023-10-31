2a2487f39dd8fe0cd4d525051246e4b768f58afb
prosper <mprosp3r@gmail.com>
2023-10-31 16:11:09 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """ making the json return """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ check the status of route """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """ check the status of route """
+    return jsonify(
+            {
+                "amenities": storage.count('Amenity'),
+                "cities": storage.count('City'),
+                "places": storage.count('Place'),
+                "reviews": storage.count('Review'),
+                "states": storage.count('State'),
+                "users": storage.count('User')
+                })
